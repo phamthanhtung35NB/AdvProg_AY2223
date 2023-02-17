26c42c8477f058e94b0be9a98c3a0374c811410a
Tùng Phạm Thanh <tungpham010203@gmail.com>
2023-02-17 23:23:34 +0700

    L1 lan test 5

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -49,7 +49,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued=='Y'||isContinued=='y'||isContinued=="countinue"||isContinued=='countinue')
+    if (isContinued=='Y'||isContinued=='y'||isContinued=='countinue')
     {
         return true;
     }
